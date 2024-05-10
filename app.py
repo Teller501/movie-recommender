e2f3b8911ad55e475b963a2f8228f4b723bc91b7
Teller501 <ande777j@stud.kea.dk>
2024-05-10 15:40:32 +0200

    updated model

--- app.py
+++ app.py
@@ -1,91 +1,99 @@
 from flask import Flask, request, jsonify
+from fastai.tabular.all import *
+from fastai.collab import *
 import pandas as pd
-import pickle
-from surprise import Dataset, Reader
-from flask import g
+import torch.nn as nn
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+load_dotenv()
 
 app = Flask(__name__)
 
-def load_model():
-    with open('model.pkl', 'rb') as file:
-        return pickle.load(file)
+# Load your trained model
+path = untar_data(URLs.ML_100k)
+learn = load_learner('movie-recommender.pkl')
 
-def save_model(model):
-    with open('model.pkl', 'wb') as file:
-        pickle.dump(model, file)
+mongo_uri = os.environ.get("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client['cinematch']
+ratings_collection = db['ratings']
 
-def get_data():
-    if 'data' not in g:
-        g.data = pd.read_csv('ratings.csv')
-    return g.data
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    algo = load_model()
-    data = request.get_json()
-    user_id = data['userId']
-    movie_id = data['movieId']
-    prediction = algo.predict(user_id, movie_id)
-    return jsonify({ 'data': prediction.est })
+def get_recommendations(user_ratings, user_id):
+    user_ratings_dicts = []
+    for movie_id, rating in user_ratings:
+        user_ratings_dicts.append({"user": user_id, "movie": movie_id, "rating": rating})
+
+    # Add or update the user's ratings in MongoDB
+    ratings_collection.update_one({"user": user_id}, {"$set": {"ratings": user_ratings_dicts}}, upsert=True)
+
+    # Load the user's ratings from MongoDB
+    user_data = ratings_collection.find_one({"user": user_id})
+    if user_data:
+        ratings = pd.DataFrame(user_data["ratings"])
+    else:
+        ratings = pd.DataFrame(columns=['user', 'movie', 'rating'])
+
+    # Add the new user's ratings to the existing dataset
+    ratings = pd.read_csv(path/'u.data', delimiter='\t', header=None, names=['user', 'movie', 'rating', 'timestamp'])
+    # Convert user_ratings_dicts to a DataFrame
+    user_ratings_df = pd.DataFrame(user_ratings_dicts)
+
+    # Append the new user's ratings to the existing dataset
+    new_ratings = pd.concat([ratings, user_ratings_df], ignore_index=True)
+
+    # Cross-tabulate the data and find the new user's row
+    crosstab = pd.crosstab(new_ratings['user'], new_ratings['movie'], values=new_ratings['rating'], aggfunc='sum').fillna(0)
+    other_users = crosstab.values[:-1]
+    new_user = crosstab.values[-1].reshape(1, -1)
+
+    # Find the top 5 most similar users using cosine similarity
+    similarities = nn.CosineSimilarity()(tensor(other_users), tensor(new_user))
+    top5 = similarities.topk(5)
+
+    # Calculate the new user's vector and bias
+    user_vectors = learn.u_weight.weight[1+top5.indices, :]
+    new_user_vector = user_vectors.mean(dim=0, keepdim=True)
+    user_biases = learn.u_bias.weight[1+top5.indices, :]
+    new_user_bias = user_biases.mean()
+
+    # Predict the new user's ratings for all movies
+    pred_ratings = torch.matmul(new_user_vector, learn.i_weight.weight.T) + learn.i_bias.weight.T + new_user_bias
+
+    # Get the top 5 movie recommendations
+    top5_ratings = pred_ratings.topk(5)
+    recommendations = [(learn.classes['title'][index], index) for index in top5_ratings.indices.tolist()[0]]
+
+    # Format recommendations as requested
+    formatted_recommendations = [{"id": id, "title": title} for title, id in recommendations]
+
+    return formatted_recommendations
 
 @app.route('/recommend', methods=['POST'])
-def recommend():
-    algo = load_model()
+def recommend_movies():
     data = request.get_json()
-    user_id = data['userId']
-    n = data.get('n', 10)
+    if data is None:
+        return jsonify({'error': 'Invalid request data'}), 400
 
-    ratings = get_data()
+    user_id = data.get('user_id')
+    user_ratings = data.get('user_ratings')
 
-    recommendations = get_top_n_recommendations(algo, ratings, user_id, n)
+    if user_id is None or user_ratings is None:
+        return jsonify({'error': 'Missing user_id or user_ratings in the request'}), 400
 
-    response = [{
-        'movieId': int(movie[0]),
-        'predictedRating': float(movie[1])
-    } for movie in recommendations]
+    if not isinstance(user_ratings, list):
+        return jsonify({'error': 'user_ratings must be a list of [movie_id, rating] pairs'}), 400
 
-    return jsonify({ 'data': response })
+    # Get the list of recommended movies
+    recommendations = get_recommendations(user_ratings, user_id)
 
-@app.route('/update_dataset', methods=['POST'])
-def update_dataset():
-    new_user_data = request.get_json()
-    new_user_id = new_user_data['userId']
-    new_user_ratings = new_user_data['ratings']
-    ratings = get_data()
-    updated_ratings = add_new_user_ratings(ratings, new_user_id, new_user_ratings)
-    g.data = updated_ratings
-
-    reader = Reader(rating_scale=(1, 5))
-    data = Dataset.load_from_df(updated_ratings[['userId', 'movieId', 'rating']], reader)
-    trainset = data.build_full_trainset()
-    algo = load_model()
-    algo.fit(trainset)
-    save_model(algo)
-
-    return jsonify({ 'data': 'Dataset and model updated successfully' })
-
-def add_new_user_ratings(ratings, new_user_id, movie_ratings):
-    new_ratings = pd.DataFrame({
-        'userId': [new_user_id] * len(movie_ratings),
-        'movieId': [mr[0] for mr in movie_ratings],
-        'rating': [mr[1] for mr in movie_ratings]
-    })
-    return pd.concat([ratings, new_ratings], ignore_index=True)
-
-def get_top_n_recommendations(algo, ratings, user_id, n=10):
-    movies = pd.read_csv('movies.csv')
-    all_movies = movies['movieId'].unique()
-    rated_movies = ratings[ratings['userId'] == user_id]['movieId'].unique()
-    unrated_movies = [movie for movie in all_movies if movie not in rated_movies]
-
-    predictions = []
-    for movie_id in unrated_movies:
-        predicted_rating = algo.predict(user_id, movie_id).est
-        predictions.append((movie_id, predicted_rating))
-
-    predictions.sort(key=lambda x: x[1], reverse=True)
-    top_n_movies = predictions[:n]
-    return top_n_movies
+    # Return the recommendations as JSON
+    return jsonify({'data': recommendations})
 
 
 if __name__ == '__main__':
